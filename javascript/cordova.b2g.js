883ff9ba7d081f9efdb97c49b52369663e133282
Gord Tanner <gtanner@gmail.com>
2013-01-26 14:30:55 -0500

    fixed hacked corodva.js file ;)

--- javascript/cordova.b2g.js
+++ javascript/cordova.b2g.js
@@ -1,6 +1,6 @@
 //Hacked, should be replaced with results from cordova-js
 setTimeout(function () {
     var evt = document.createEvent('Events');
-    evt.init("deviceready", false , false);
+    evt.initEvent("deviceready", false , false);
     document.dispatchEvent(evt);
 }, 10);
